8c591df91407b31235f0c7143b55b61e2365a452
Piotr Sowa <piotrsowa87@gmail.com>
2023-10-27 09:06:58 +0200

    Add rules to the game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
 
-printMessage('Zagrałem ' + computerMove + '! Twój ruch to ' + playerMove + ', więc wygrywasz!');
+let computerMove = 'nieznany ruch';
+let playerMove = 'nieznany ruch';
+
+computerMove = getMoveName(Math.floor(Math.random()*3+1));
+playerMove = getMoveName(prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'));
+
+console.log('moves:', computerMove, playerMove);
+
+if (playerMove != 'nieznany ruch') {
+    displayResults(computerMove, playerMove);
+} else {
+    printMessage("Spróbuj jeszcze raz!");
+}
+
+/* Functions */
+function getMoveName(argMoveId) {
+    if (argMoveId == 1) return 'kamień';
+    if (argMoveId == 2) return 'papier';
+    if (argMoveId == 3) return 'nożyce';
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch'
+}
+
+/**** Rules  ****
+* Kamień (1) > Nożyce (3)
+* Nozyce (3) > Papier (2)
+* Papier (2) > Kamień (1)
+*/
+function displayResults(argComputerMove, argPlayerMove) {
+    printMessage('Zagrałem ' + argComputerMove
+    + '! Twój ruch to ' + argPlayerMove + '!');
+
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+    } else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+        printMessage('Ty wygrywasz!');
+    } else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+    } else {
+        printMessage('Tym razem przegrywasz :(');
+    }
+}
