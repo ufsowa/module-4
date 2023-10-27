084c3431d4ff81af9a9089d8753949f8c0fdaad9
Piotr Sowa <piotrsowa87@gmail.com>
2023-10-27 09:15:18 +0200

    Add more results

--- js/script.js
+++ js/script.js
@@ -31,11 +31,11 @@ function displayResults(argComputerMove, argPlayerMove) {
     printMessage('Zagrałem ' + argComputerMove
     + '! Twój ruch to ' + argPlayerMove + '!');
 
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-        printMessage('Ty wygrywasz!');
-    } else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-        printMessage('Ty wygrywasz!');
-    } else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    if (argPlayerMove == argComputerMove) {
+        printMessage('Remis!');
+    } else if( argComputerMove == 'kamień' && argPlayerMove == 'papier' ||
+        argComputerMove == 'papier' && argPlayerMove == 'nożyce' ||
+        argComputerMove == 'nożyce' && argPlayerMove == 'kamień' ){
         printMessage('Ty wygrywasz!');
     } else {
         printMessage('Tym razem przegrywasz :(');
