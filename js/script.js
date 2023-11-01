19502a64c56a9327585609800ab7017e8d5ae4af
Piotr Sowa <piotrsowa87@gmail.com>
2023-11-01 10:52:32 +0100

    Update variables

--- js/script.js
+++ js/script.js
@@ -1,77 +1,85 @@
-let computerCounter = 0;
-let playerCounter = 0;
-document.getElementById('btn-reset').addEventListener('click', function(event) {resetGame()});
-document.getElementById('btn-test').addEventListener('click', function(event) {testResults()});
+{
+    let computerCounter = 0;
+    let playerCounter = 0;
 
-document.getElementById('btn-rock').addEventListener('click', function(event) {playGame(1); event.stopPropagation()});
-document.getElementById('btn-paper').addEventListener('click', function(event) {playGame(2); event.stopPropagation()});
-document.getElementById('btn-scissors').addEventListener('click', function(event) {playGame(3); event.stopPropagation()});
-document.body.addEventListener('click', function() {clearMessages()});
+    document.getElementById('btn-reset').addEventListener('click', function(event) {resetGame()});
+    document.getElementById('btn-test').addEventListener('click', function(event) {testResults()});
 
+    document.getElementById('btn-rock').addEventListener('click', function(event) {playGame(1); event.stopPropagation()});
+    document.getElementById('btn-paper').addEventListener('click', function(event) {playGame(2); event.stopPropagation()});
+    document.getElementById('btn-scissors').addEventListener('click', function(event) {playGame(3); event.stopPropagation()});
+    document.body.addEventListener('click', function() {clearMessages()});
 
-/* Functions */
-function testResults(trialsNumber = 10) {
-    while (trialsNumber > 0) {
-        playGame(3);
-        trialsNumber--;
-    }
-}
-
-function resetGame() {
-    clearResults();
-    computerCounter = 0;
-    playerCounter = 0;
-}
-
-function playGame(playerInput){
-    let computerMove = getMoveName(Math.floor(Math.random()*3+1));
-    let playerMove = getMoveName(playerInput);
-
-    clearMessages();
-    
-    if (!isPlayerWiner(computerMove, playerMove)){
-        if (Math.random() < 0.75){
-            computerMove = getMoveName(Math.floor(Math.random()*3+1));
+    /* Methods */
+    const testResults = function (trialsNumber = 10) {
+        while (trialsNumber > 0) {
+            playGame(getRandomChoice());
+            trialsNumber--;
         }
     }
 
-    console.log('moves:', computerMove, playerMove);
-
-    displayResults(computerMove, playerMove);
-}
-
-function getMoveName(argMoveId) {
-    if (argMoveId == 1) return 'kamień';
-    if (argMoveId == 2) return 'papier';
-    if (argMoveId == 3) return 'nożyce';
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch'
-}
-
-/**** Rules  ****
-* Kamień (1) > Nożyce (3)
-* Nozyce (3) > Papier (2)
-* Papier (2) > Kamień (1)
-*/
-function displayResults(argComputerMove, argPlayerMove) {
-    printMessage('Zagrałem ' + argComputerMove
-    + '! Twój ruch to ' + argPlayerMove + '!');
-
-    if (argPlayerMove == argComputerMove) {
-        printMessage('Remis!');
-    } else if(isPlayerWiner(argComputerMove, argPlayerMove)){
-        printMessage('Ty wygrywasz!', ['result','success']);
-        playerCounter++;
-    } else {
-        printMessage('Tym razem przegrywasz :(', ['result','failure']);
-        computerCounter++;
+    const resetGame = function () {
+        clearResults();
+        computerCounter = 0;
+        playerCounter = 0;
     }
 
-    printResult(computerCounter, playerCounter);
-}
+    const playGame = function (playerInput){
+        let computerMove = getMoveName(getRandomChoice());
+        let playerMove = getMoveName(playerInput);
 
-function isPlayerWiner(argComputerMove, argPlayerMove){
-    return argComputerMove == 'kamień' && argPlayerMove == 'papier' ||
-    argComputerMove == 'papier' && argPlayerMove == 'nożyce' ||
-    argComputerMove == 'nożyce' && argPlayerMove == 'kamień';
+        clearMessages();
+    
+        if (!isPlayerWiner(computerMove, playerMove)){
+            if (Math.random() < 0.75){
+                computerMove = getMoveName(getRandomChoice());
+            }
+        }
+
+        console.log('moves:', computerMove, playerMove);
+
+        displayResults(computerMove, playerMove);
+    }
+
+    const getMoveName = function (argMoveId) {
+        if (argMoveId == 1) return 'kamień';
+        if (argMoveId == 2) return 'papier';
+        if (argMoveId == 3) return 'nożyce';
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch'
+    }
+
+    /**** Rules  ****
+    * Kamień (1) > Nożyce (3)
+    * Nozyce (3) > Papier (2)
+    * Papier (2) > Kamień (1)
+    */
+    const displayResults = function (argComputerMove, argPlayerMove) {
+        printMessage('Zagrałem ' + argComputerMove
+        + '! Twój ruch to ' + argPlayerMove + '!');
+
+        if (argPlayerMove == argComputerMove) {
+            printMessage('Remis!');
+        } else if(isPlayerWiner(argComputerMove, argPlayerMove)){
+            printMessage('Ty wygrywasz!', ['result','success']);
+            playerCounter++;
+        } else {
+            printMessage('Tym razem przegrywasz :(', ['result','failure']);
+            computerCounter++;
+        }
+
+        printResult(computerCounter, playerCounter);
+    }
+
+    /* Utilities */
+    function isPlayerWiner(argComputerMove, argPlayerMove){
+        return argComputerMove == 'kamień' && argPlayerMove == 'papier' ||
+        argComputerMove == 'papier' && argPlayerMove == 'nożyce' ||
+        argComputerMove == 'nożyce' && argPlayerMove == 'kamień';
+    }
+
+    function getRandomChoice() {
+        return Math.floor(Math.random()*3+1);
+    }
+
 }
